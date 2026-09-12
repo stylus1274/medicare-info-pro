2d19d0e34d1b3dbf2eb2c010fc2f7e7bbee80a9c
stylus1274 <6.390707e+06+stylus1274@users.noreply.github.com>
2026-09-12 11:54:19 +0000

    Add ANOC guide to post sitemap

--- app/post-sitemap.xml/route.ts
+++ app/post-sitemap.xml/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 const BASE_URL = "https://medicareinfopro.com";
 
 const BLOG_SLUGS = [
+  "/how-to-read-medicare-annual-notice-of-change",
   "/medicare-prior-authorization-appeals",
   "/does-medicare-cover-zepbound",
   "/does-medicare-cover-wegovy",
